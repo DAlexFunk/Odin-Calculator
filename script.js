4cb5840f2ea95a398690cbd1acee46b229aac582
Alex Funk <alexdagreat04@gmail.com>
2024-09-05 18:40:16 -0400

    Partial implementation of the buttons
    Does do some calculations. Must press equals after each calculation.
    Known bugs: Cannot chain calculations, large numbers overflow off the screen. Floating point precision D:

--- script.js
+++ script.js
@@ -22,27 +22,69 @@ function operate(num1, num2, operator) {
     } else if (operator === "*") {
         return multiply(num1, num2);
     } else if (operator === "/") {
-        return divide(num1, num2);
+        if (num2 === 0) {
+            return "bro?";
+        } else {
+            return divide(num1,num2);
+        }
     }
     return "ERROR";
 }
 
 let num1 = "";
 let num2 = "";
-let operator = "";
 let result = "";
+let operator = "";
 
 const numberButtons = document.querySelectorAll(".number");
 const clearButton = document.querySelector("#clear");
 const backButton = document.querySelector("#back");
 const screen = document.querySelector(".screen");
+const addButton = document.querySelector("#plus");
+const subtractButton = document.querySelector("#minus");
+const multiplyButton = document.querySelector("#multiply");
+const divideButton = document.querySelector("#divide");
+const equalsButton = document.querySelector("#enter");
 
-clearButton.addEventListener("click", () => screen.textContent = "");
+
+clearButton.addEventListener("click", () => {
+    screen.textContent = "";
+    operator = "";
+    num1 = "";
+});
 
 backButton.addEventListener("click", () => screen.textContent = screen.textContent.slice(0, screen.textContent.length - 1));
 
-numberButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        screen.textContent += button.textContent;
-    });
-});
+numberButtons.forEach((button) => button.addEventListener("click", () => {
+    if (screen.textContent.length < 10) screen.textContent += button.textContent;
+}));
+
+addButton.addEventListener("click", () => {
+    num1 = Number(screen.textContent);
+    operator = "+";
+    screen.textContent = "";
+})
+
+subtractButton.addEventListener("click", () => {
+    num1 = Number(screen.textContent);
+    operator = "-";
+    screen.textContent = "";
+})
+
+multiplyButton.addEventListener("click", () => {
+    num1 = Number(screen.textContent);
+    operator = "*";
+    screen.textContent = "";
+})
+
+divideButton.addEventListener("click", () => {
+    num1 = Number(screen.textContent);
+    operator = "/";
+    screen.textContent = "";
+})
+
+equalsButton.addEventListener("click", () => {
+    result = operate(Number(num1), Number(screen.textContent), operator);
+    num1 = result;
+    screen.textContent = result;
+})
